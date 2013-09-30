e0766cdd5014abd5a6a5523d85e6537d616b4705
Fremen DeRuvo <fremenderuvo@gmail.com>
2013-09-30 16:48:15 -0500

    add respond_to json for index and show'

--- app/controllers/wardrobe_items_controller.rb
+++ app/controllers/wardrobe_items_controller.rb
@@ -1,9 +1,13 @@
 class WardrobeItemsController < ApplicationController
+
+  respond_to :html, :json
   def index
     @wardrobe_items = WardrobeItem.all
+    respond_with(@wardrobe_items)
   end
 
   def show
     @wardrobe_item = WardrobeItem.find(params[:id])
+    respond_with(@wardrobe_item)
   end
 end
