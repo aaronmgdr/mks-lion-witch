8b81232427f2d54ba6ae1c3aea610b2d1817ba39
leejaew <lee.jaewang@gmail.com>
2013-09-30 15:40:39 -0500

    create route based on controller and add view template

---
+++ spec/controllers/wardrobe_items_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe WardrobeItemsController do
+  describe 'GET index' do
+    it "assigns all wardrobe items as @wardrobe_items" do
+      # wardrobe_item = WardrobeItem.new
+      # wardrobe_item.save
+      wardrobe_item = WardrobeItem.create
+      get :index
+      
+      # expect(assigns 'wardrobe_items').to eq([wardrobe_item])
+    end
+  end
+end
