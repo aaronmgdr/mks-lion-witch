f667e0de2f21025068ccfd4c836b00ed2c454eaa
leejaew <lee.jaewang@gmail.com>
2013-09-30 16:42:52 -0500

    create test to display information about a single wardrobe item

--- spec/controllers/wardrobe_items_controller_spec.rb
+++ spec/controllers/wardrobe_items_controller_spec.rb
@@ -1,14 +1,24 @@
 require 'spec_helper'
 
 describe WardrobeItemsController do
+
+  let(:wardrobe_item) { wardrobe_item = WardrobeItem.create }
+
   describe 'GET index' do
     it "assigns all wardrobe items as @wardrobe_items" do
       # wardrobe_item = WardrobeItem.new
       # wardrobe_item.save
-      wardrobe_item = WardrobeItem.create
+      # wardrobe_item = WardrobeItem.create # => use let()
       get :index
-      
-      expect(assigns ('wardrobe_items')).to eq([wardrobe_item])
+      expect(assigns('wardrobe_items')).to eq([wardrobe_item])
     end
   end
+
+  describe 'GET show' do
+    it "display information about a single wardrobe item" do
+      get :show, { id: wardrobe_item.id }
+      expect(assigns('wardrobe_item')).to eq(wardrobe_item)
+    end
+  end
+
 end
